b0323d727761e8e310521f22ea4b8a09f250b8bd
Yogeshwar Vhatkar <yogeshwar.vhatkar@bnt-soft.com>
2025-04-15 17:33:00 +0530

    add components with stylesheet and routing
    Day 2 learning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"dashboard", component:DashboardComponent },
+  { path:"first", component:FirstComponent },
+  { path:"second", component:SecondComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FirstChildComponent } from './first/first-child/first-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    DashboardComponent,
+    FirstChildComponent
   ],
   imports: [
     BrowserModule,
